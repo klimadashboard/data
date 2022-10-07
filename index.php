a1094d9ac7d96c2c2b813d93c908ebeae8e3a570
Data Server <data@klimadashboard.org>
2022-10-07 09:59:03 +0000

    automatic data commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Data Server – Klimadashboard</title>
+</head>
+<body>
+<h1>data.klimadashboard.org</h1>
+
+<p>This is the data server for Klimadashboard.org, containing the files we use for our visualisations.<br>
+Please refer to <a href="https://github.com/klimadashboard/data">our GitHub-Repository</a> for more information.</p>
+
+<?php
+$files = array_diff(scandir($path), array('.', '..'));
+?>
+
+<ul>
+<?php foreach($files as $file): ?>
+<li><?= $file ?></li>
+<?php endforeach ?>
+</ul>
+
+<style>
+body {
+	padding-top: 2em;
+	max-width: 60ch;
+	margin: 0 auto;
+	font-family: sans-serif;
+}
+</style>
+</body>
+</html>
